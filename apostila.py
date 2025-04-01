4e754b6d20b8791b893c700bb85758115921ceeb
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2025-04-01 19:59:06 +0000

    comcluido 1

--- apostila.py
+++ apostila.py
@@ -16,7 +16,7 @@ class Jogo:
 class Inimigo:
     def __init__(self):
         self.nome = "Monstrinho"
-        self.vida = None
+        self.vida = 100
 
     def definir_vida(self, quantidade):
         self.vida = quantidade
@@ -90,7 +90,7 @@ class Menu:
         game.iniciar()
     
     def mostrar_opcoes(self):
-        print("Modos de Jogo:\nFacil\nMédio\nDificil")
+        print("Modos de Jogo:\n1: Facil\n2: Médio\n3: Dificil")
     
     def sair(self):
         print("O Jogo foi Fechado")
@@ -110,24 +110,46 @@ mob = Personagem()
 bixo = Inimigo()
 
 while True:
+    acao = int(input(f"Menu: digite o valor da ação\n1: Iniciar Jogo\n2: Mostrar Opções\n3: Sair\n"))
+    if acao == 2:
+        print("Escolha a opção com base no valor")
+        menu.mostrar_opcoes()
+        dificul = int(input())
+        match dificul:
+            case 1:
+                bixo.definir_vida(50)
+            case 2:
+                bixo.definir_vida(100)
+            case 3:
+                bixo.definir_vida(200)
+    elif acao == 1:
+        menu.iniciar_jogo(game)
+        inimigos = random.randint(1,3)
+        derrotados = 0
+        while mob.vida > 0 and bixo.vida > 0 and derrotados != inimigos:
+            acao = int(input(f"Você tem {mob.vida} pontos de vida e {player.energia} pontos de energia\nO que vai fazer: digite o valor da ação\n1: Atacar\n2: Descansar\n"))
+            if acao == 1 and player.energia > 0:
+                dano = random.randint(5,20)
+                player.atacar(bixo, dano)
+                if bixo.vida <= 0:
+                    print(f"O {bixo.nome} foi derrotado")
+                    pontos.adicionar_pontos(10)
+                    bixo.vida=100
+                    derrotados+=1
+                dano = random.randint(5,20)
+                bixo.atacar(mob, dano)
+                if mob.vida <= 0:
+                    print(f"O {mob.nome} foi derrotado")
+                    mob.fim_de_jogo()
+                    break
+            elif acao == 1 and player.energia <= 0:
+                print(f"O {mob.nome} esta sem energia para atacar")
+            
+            elif acao == 2:
+                player.recuperar_energia(20)
+    elif acao == 3:
+        menu.sair()
+        break
+    else:
+        print("Ação invalida")
     
-    while mob.vida > 0 and bixo.vida > 0:
-        acao = int(input(f"Você tem {mob.vida} pontos de vida e {player.energia} pontos de energia\nO que vai fazer: digite o valor da ação\n1: Atacar\n2: Descansar\n"))
-        if acao == 1 and player.energia > 0:
-            dano = random.randint(5,20)
-            player.atacar(bixo, dano)
-            if bixo.vida <= 0:
-                print(f"O {bixo.nome} foi derrotado")
-                pontos.adicionar_pontos(10)
-                bixo.vida=100
-            dano = random.randint(5,20)
-            bixo.atacar(mob, dano)
-            if mob.vida <= 0:
-                print(f"O {mob.nome} foi derrotado")
-                mob.fim_de_jogo()
-                break
-        elif acao == 1 and player.energia <= 0:
-            print(f"O {mob.nome} esta sem energia para atacar")
-        
-        elif acao == 2:
-            player.recuperar_energia(20)
